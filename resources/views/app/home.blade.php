1b58466ef2ecb9d2884d0f465dde045aa6f40bca
MKashanButt <kashanbutt566@gmail.com>
2025-09-05 20:36:31 +0500

    add home section

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,1 +1,39 @@
-<x-app-layout></x-app-layout>
+<x-app-layout>
+    <main>
+        <section class="flex flex-col gap-8 items-center justify-center h-screen" id="home">
+            <div>
+                <h1 class="text-4xl font-heavy tracking-normal">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit.
+                    Reprehenderit, aliquam?
+                </h1>
+                <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
+                    praesentium
+                    asperiores minus odio
+                    provident error autem qui voluptates beatae repudiandae culpa ea illo rerum natus, ad
+                    quo,
+                    consequatur recusandae eveniet.</p>
+            </div>
+            <div class="flex gap-8">
+                <img src="https://placehold.co/400x300" alt="" class="rounded-3xl">
+                <img src="https://placehold.co/400x300" alt="" class="rounded-3xl">
+                <img src="https://placehold.co/740x300" alt="" class="rounded-3xl">
+            </div>
+            <div class="w-[84%] flex justify-end gap-4">
+                <button class="p-2 bg-yellow-500 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-5">
+                        <path d="M6 8L2 12L6 16" />
+                        <path d="M2 12H22" />
+                    </svg>
+                </button>
+                <button class="p-2 bg-yellow-500 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-5">
+                        <path d="M18 8L22 12L18 16" />
+                        <path d="M2 12H22" />
+                    </svg>
+                </button>
+            </div>
+        </section>
+    </main>
+</x-app-layout>
