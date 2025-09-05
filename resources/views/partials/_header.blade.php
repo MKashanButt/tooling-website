ec77572fe8163cd1d84009d982df4918c510e234
MKashanButt <kashanbutt566@gmail.com>
2025-09-05 20:35:15 +0500

    add header

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,1 +1,47 @@
-<header></header>
+<header class="w-full bg-gray-100 fixed top-0 left-0">
+    <div class="w-full mx-auto flex gap-2 items-center py-4 select-none">
+        <div class="p-2 flex items-center justify-center">
+            <h1 class="text-4xl tracking-widest font-bold uppercase">Logo.</h1>
+        </div>
+        <nav class="flex flex-1 justify-end gap-4">
+            <a href="#home">
+                <div
+                    class="p-2 flex items-center justify-center gap-3 text-gray-600 stroke-gray-600 hover:bg-yellow-500 hover:text-black transition ease-in-out duration-150 rounded-full px-6">
+                    <p>Home</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5">
+                        <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                        <path
+                            d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    </svg>
+                </div>
+            </a>
+            <a href="#tools">
+                <div
+                    class="p-2 flex items-center justify-center gap-3 text-gray-600 stroke-gray-600 hover:bg-yellow-500 hover:text-black transition ease-in-out duration-150 rounded-full px-6">
+                    <p>Tools</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5">
+                        <path d="M10 10V5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5" />
+                        <path d="M14 6a6 6 0 0 1 6 6v3" />
+                        <path d="M4 15v-3a6 6 0 0 1 6-6" />
+                        <rect x="2" y="15" width="20" height="4" rx="1" />
+                    </svg>
+                </div>
+            </a>
+            <a href="#account">
+                <div
+                    class="p-2 flex items-center justify-center gap-3 text-gray-600 hover:bg-yellow-500 hover:text-black transition ease-in-out duration-150 rounded-full px-6">
+                    <p>Account</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 stroke-gray-600">
+                        <path
+                            d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
+                        <path d="M6.376 18.91a6 6 0 0 1 11.249.003" />
+                        <circle cx="12" cy="11" r="4" />
+                    </svg>
+                </div>
+            </a>
+        </nav>
+    </div>
+</header>
